ce7acec37ab6629383de9cc3b0fc1ff51281e6c9
Robin Houston <robin.houston@gmail.com>
2012-08-02 16:44:21 +0100

    Encapsulate the client in a class

--- bin/client.rb
+++ bin/client.rb
@@ -3,19 +3,25 @@
 require "socket"
 
 # Ruby client code
-@socket = UNIXSocket.new("/tmp/.runner-sock")
-def cmd(args)
-  @socket.puts args.join("\0")
-
-  status_line = @socket.gets
-  status_line =~ /^(\d\d\d) (\d+) (.*)\n/
-  status, message = $1, $3
-  text = @socket.read($2.to_i)
+class Client
+  def initialize(socket_path)
+    @socket = UNIXSocket.new(socket_path)
+  end
   
-  return status, message, text
+  def cmd(args)
+    @socket.puts args.join("\0")
+    
+    status_line = @socket.gets
+    status_line =~ /^(\d\d\d) (\d+) (.*)\n/
+    status, message = $1, $3
+    text = @socket.read($2.to_i)
+    
+    return status, message, text
+  end
 end
 
 # Main program
-status, message, text = cmd(ARGV)
+client = Client.new("/tmp/.runner-sock")
+status, message, text = client.cmd(ARGV)
 puts "#{status} #{message}"
 puts text if !text.empty?
